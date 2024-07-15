4b9f13399f6dc600627ae856ab2262574826c21c
Khyosunny <kimhyo_0218@naver.com>
2024-07-15 22:44:04 +0900

    refactor: optimistic update 제거

--- src/hooks/useFavoriteSellerController.ts
+++ src/hooks/useFavoriteSellerController.ts
@@ -1,89 +1,17 @@
-import { AxiosError } from 'axios';
-import {
-  InfiniteData,
-  useMutation,
-  useQueryClient,
-} from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteFavoriteSeller, postFavoriteSeller } from '../apis';
-import { ProductType, ResponseType } from '../types/products';
 
 const useFavoriteSellerController = (queryKey: ReadonlyArray<unknown>) => {
   const queryClient = useQueryClient();
 
-  const getPreviousProductList = () => {
-    const previousQueryData =
-      queryClient.getQueryData<InfiniteData<ResponseType<ProductType[]>>>(
-        queryKey,
-      );
-    const previousProductList =
-      previousQueryData?.pages.flatMap((page) => page.result) || [];
-
-    return previousProductList;
-  };
-
-  const updateFavoriteInCache = (sellerId: string, isFavorite: boolean) => {
-    queryClient.setQueryData<InfiniteData<ResponseType<ProductType[]>>>(
-      queryKey,
-      (oldData) => {
-        if (!oldData) return oldData;
-
-        const newPages = oldData.pages.map((page) => {
-          const newResult = page.result.map((item) => {
-            if (item.seller === sellerId) {
-              return { ...item, favorite: isFavorite };
-            }
-            return item;
-          });
-          return { ...page, result: newResult };
-        });
-
-        return { ...oldData, pages: newPages };
-      },
-    );
-  };
-
-  const onMutate = async (sellerId: string, isFavorite: boolean) => {
-    await queryClient.cancelQueries({ queryKey });
-    const previousProductList = getPreviousProductList();
-
-    updateFavoriteInCache(sellerId, isFavorite);
-
-    return { previousProductList };
-  };
-
-  const onError = (
-    err: AxiosError,
-    sellerId: string,
-    context: { previousProductList: ProductType[] | undefined } | undefined,
-    isFavorite: boolean,
-  ) => {
-    if (context?.previousProductList) {
-      updateFavoriteInCache(sellerId, !isFavorite);
-    }
-  };
-
-  const { mutate: postFavoriteSellerMutate } = useMutation<
-    string,
-    AxiosError,
-    string,
-    { previousProductList: ProductType[] | undefined }
-  >({
+  const { mutate: postFavoriteSellerMutate } = useMutation({
     mutationFn: postFavoriteSeller,
-    onMutate: (sellerId) => onMutate(sellerId, true),
     onSuccess: () => queryClient.invalidateQueries({ queryKey }),
-    onError: (err, sellerId, context) => onError(err, sellerId, context, true),
   });
 
-  const { mutate: deleteFavoriteSellerMutate } = useMutation<
-    string,
-    AxiosError,
-    string,
-    { previousProductList: ProductType[] | undefined }
-  >({
+  const { mutate: deleteFavoriteSellerMutate } = useMutation({
     mutationFn: deleteFavoriteSeller,
-    onMutate: (sellerId) => onMutate(sellerId, false),
     onSuccess: () => queryClient.invalidateQueries({ queryKey }),
-    onError: (err, sellerId, context) => onError(err, sellerId, context, false),
   });
 
   const onToggleFavorite = (sellerId: string, favorite: boolean) => {
